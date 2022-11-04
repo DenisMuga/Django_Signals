1e318686d42424c531ddce502ccdc0a55806a701
DenisMuga <denismugah5@gmail.com>
2022-11-04 13:27:21 +0300

    Implementing signals using receiver

--- cobol/models.py
+++ cobol/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -9,3 +11,14 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+# def task_handler(sender, instance, **kwargs):
+#     print('We are implementing django signals')
+#     print(instance.name)
+#     print(instance.description)
+
+
+# pre_save.connect(task_handler, sender=Task)
+
+
+
