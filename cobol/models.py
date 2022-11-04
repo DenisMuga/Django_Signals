b5240bdda5d90224da48145696ca55041967a1ba
DenisMuga <denismugah5@gmail.com>
2022-11-04 14:32:21 +0300

    Creating a new TaskDate model

--- cobol/models.py
+++ cobol/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.template.defaultfilters import slugify
+from datetime import datetime
 
 # Create your models here.
 
@@ -13,6 +14,10 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+class TaskDate(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    date = models.CharField(max_length=100)
 
 # def task_handler(sender, instance, **kwargs):
 #     print('We are implementing django signals')
@@ -26,8 +31,12 @@ class Task(models.Model):
 @receiver(pre_save, sender=Task)
 def task_handler(sender, instance, **kwargs):
     print('Writing signals Using receiver')
-    print(instance.name)
-    print(instance.description)
+    print(slugify(instance.name))
+    instance.slug = (slugify(instance.name))
+    print(slugify(instance.description))
 
+@receiver(post_save, sender=Task)
+def task_handler_post (sender, instance, **kwargs):
+    TaskDate.objects.create(task=instance, date = datetime.now())
 
 
